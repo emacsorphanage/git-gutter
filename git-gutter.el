dd3981f71751926bd49c483e2770347a6b430023
Syohei YOSHIDA <syohex@gmail.com>
2013-02-15 19:03:32 +0900

    Bump up version and update Changes

--- git-gutter.el
+++ git-gutter.el
@@ -4,7 +4,7 @@
 
 ;; Author: Syohei YOSHIDA <syohex@gmail.com>
 ;; URL: https://github.com/syohex/emacs-git-gutter
-;; Version: 0.14
+;; Version: 0.15
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
